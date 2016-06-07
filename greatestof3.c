37a29e8e3eb6b2eb382121c53dceeee721578e8d
poojachandran <pooja.poojachandran29@gmail.com>
2016-06-07 11:40:15 +0530

    Create greatestof3.c

---
+++ greatestof3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+if(a>b && a>c)
+	printf("A is greater");
+else if(b>a && b>c)
+	printf("B is greater");
+else
+	printf("C is greater");
+return 0;
+}
